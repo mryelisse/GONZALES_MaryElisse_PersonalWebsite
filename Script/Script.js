b7a454652d2cba44041545dea814652b54bc5994
mryelisse <maryelissegonzales@gmail.com>
2017-10-04 14:12:22 +0800

    Created a slider for my portfolio page

--- Script/Script.js
+++ Script/Script.js
@@ -13,3 +13,15 @@ function setBindings() {
 			}, 1000)
 		})
 }
+
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  3000);
